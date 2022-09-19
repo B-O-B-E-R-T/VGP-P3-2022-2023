881797cd511d285ba93efc797e578b09ec2dc789
B-O-B-E-R-T <101837310+B-O-B-E-R-T@users.noreply.github.com>
2022-09-19 10:10:15 -0700

    vehicle move part 2

--- CreateWithCode/Prototype 1/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //We'll move the vehicle forward
-        transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
 
     }
 }
