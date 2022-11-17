d859b3af1c61414f5f445645f2a32a342c4b2515
B-O-B-E-R-T <101837310+B-O-B-E-R-T@users.noreply.github.com>
2022-11-17 10:34:41 -0800

    Player input and adjusting gravity and jump force

--- CreateWithCode/Prototype3/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype3/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpForce;
+    public float GravityModifier;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerRb.AddForce(Vector3.up * 100);
+        Physics.gravity *= GravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)){
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
