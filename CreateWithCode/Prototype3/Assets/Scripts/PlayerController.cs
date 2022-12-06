c57e4064e2e3c711b44d6304afd2f76cd2c1af06
B-O-B-E-R-T <101837310+B-O-B-E-R-T@users.noreply.github.com>
2022-12-06 10:37:54 -0800

    animations for running, jumping, game over

--- CreateWithCode/Prototype3/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype3/Assets/Scripts/PlayerController.cs
@@ -14,15 +14,17 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= GravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround){
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver){
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision) {
@@ -31,6 +33,8 @@ public class PlayerController : MonoBehaviour
         } else if (collision.gameObject.CompareTag("Obstacle")){
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
         
     }
