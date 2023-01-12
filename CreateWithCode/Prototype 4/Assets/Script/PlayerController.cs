2a9cbd9881118c2a629beffe844cd25fc3dc73b6
B-O-B-E-R-T <101837310+B-O-B-E-R-T@users.noreply.github.com>
2023-01-12 10:36:09 -0800

    power up stuff

--- CreateWithCode/Prototype 4/Assets/Script/PlayerController.cs
+++ CreateWithCode/Prototype 4/Assets/Script/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public GameObject powerupIndicator;
     private float powerupStrength = 15.0f;
     public float speed = 5.0f;
     public bool hasPowerup;
@@ -24,14 +25,24 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
     }
 
     private void OnTriggerEnter(Collider other){
         if (other.CompareTag("Powerup")){
             hasPowerup = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+            powerupIndicator.gameObject.SetActive(true);
         }
     }
+
+    IEnumerator PowerupCountdownRoutine(){
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
     private void OnCollisionEnter(Collision collision){
         if (collision.gameObject.CompareTag("Enemy") && hasPowerup){
 
@@ -40,7 +51,7 @@ public class PlayerController : MonoBehaviour
 
             Debug.Log("Collided with " + collision.gameObject.name + " with powerup set up " + hasPowerup);
 
-            
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
         }
     }
 }
