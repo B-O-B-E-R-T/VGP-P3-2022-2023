7b8f9adaf0e8e4d1b344bf2972712a5934f847db
B-O-B-E-R-T <101837310+B-O-B-E-R-T@users.noreply.github.com>
2023-02-02 13:49:25 -0800

    game over things

--- CreateWithCode/Prototype 5/Assets/Course Library/Scripts/GameManager.cs
+++ CreateWithCode/Prototype 5/Assets/Course Library/Scripts/GameManager.cs
@@ -7,6 +7,8 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
     // Start is called before the first frame update
@@ -15,6 +17,7 @@ public class GameManager : MonoBehaviour
         StartCoroutine(SpawnTarget());
         score = 0; 
         UpdateScore(0);
+        isGameActive = true;
     }
 
     // Update is called once per frame
@@ -24,7 +27,7 @@ public class GameManager : MonoBehaviour
     }
 
     IEnumerator SpawnTarget(){
-        while(true){
+        while(isGameActive){
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
@@ -34,5 +37,9 @@ public class GameManager : MonoBehaviour
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
 
+    }
+    public void GameOver(){
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
     }
 }
